d14e8448c591e707faebb3b6c8699622f5eb2adc
myly100 <cs.tajaly@gmail.com>
2024-06-05 19:00:56 +0200

    create idle

--- scripts/enemyKill.gd
+++ scripts/enemyKill.gd
@@ -3,6 +3,7 @@ extends CollisionShape2D
 @onready var timer = $"../Timer"
 
 func _on_enemy_kill_body_entered(body):
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 func _on_timer_timeout():
--- scripts/kill.gd
+++ scripts/kill.gd
@@ -4,11 +4,13 @@ extends CollisionShape2D
 @onready var timer = $"../Timer"
 
 func _on_area_2d_body_entered(body):
-	
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 	
 
 
 func _on_timer_timeout():
 	print("you died")
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
